248cddc98a6ff8686e85d809c8689113aa7b73a4
TheJan-Jan <62783500+TheJan-Jan@users.noreply.github.com>
2022-09-19 14:10:36 +0800

    Update diy-part2.sh

--- x86/diy-part2.sh
+++ x86/diy-part2.sh
@@ -11,4 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.0.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.10/g' package/base-files/files/bin/config_generate
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
